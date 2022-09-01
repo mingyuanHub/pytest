855eb307b0f76f211ec0032e0c187d3c4401c5b2
mingyuan <margin.ma@tradplus.com>
2022-09-01 14:41:07 +0800

    feat：yaml 写入

--- yaml/v1/v1.py
+++ yaml/v1/v1.py
@@ -1,12 +1,20 @@
 import yaml
 
 if __name__ == '__main__':
+    # 读取yml内容
     with open("data.yml", encoding="utf-8") as f:
         result = f.read()
         x = yaml.load(result, Loader=yaml.FullLoader)
     print(x)
 
+    # 读取json内容
     with open("json.yml", encoding="utf-8") as f:
         result = f.read()
         x = yaml.load(result, Loader=yaml.FullLoader)
     print(x)
+
+    # 写入yml内容
+    x['composition']['1999'].append("许三观卖血记")
+    with open("w.yml", 'w', encoding='utf-8') as w_f:
+        # 覆盖原先的配置文件
+        yaml.dump(x, w_f, allow_unicode=True)
